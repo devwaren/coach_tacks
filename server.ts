51a046cf5fd458cd30f78a2c4010cf2588465e5d
Waren Gador <waren.gador15@gmail.com>
2025-09-03 11:57:42 +0800

    update

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url'
 import express from 'express'
 import type { ViteDevServer } from 'vite'
 import crypto from 'crypto' // added for nonce
-import { runServer } from './index.ts' // import the backend server
 
 // Constants
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
